c54fdb7df30b58d4f1939e75394bda33e0702efb
Ilya <ilyak@playtika.com>
2020-04-21 23:53:56 +0300

    binpack demo

--- packages/binpack/demo/rollup.config.js
+++ packages/binpack/demo/rollup.config.js
@@ -1,12 +1,15 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
+import {terser} from 'rollup-plugin-terser';
 
 export default {
   input: 'dist/esm/index.js',
   output: {
     file: 'dist/index.js',
     format: 'iife',
-    name: 'demo',
-    sourcemap: true
+    name: 'binpack',
+    sourcemap: true,
+    compact: true,
+    plugins: [terser()]
   },
   plugins: [
     nodeResolve()
